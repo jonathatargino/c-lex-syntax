b30981aea68b8730041f71ae7a7c78d57f75760a
Jonatha Targino <j.oliveiratargino@hotmail.com>
2025-12-04 16:57:07 -0300

    chore: add specific examples for each tokenization situation

--- code_examples/conditional.c
+++ code_examples/conditional.c
@@ -1,25 +1,5 @@
-#include <stdio.h>
-
-int main() {
-    int opcao;
-
-    printf("Digite um numero (1 a 3): ");
-    scanf("%d", &opcao);
-
-    switch (opcao) {
-        case 1:
-            printf("Opcao 1 escolhida\n");
-            break;
-        case 2:
-            printf("Opcao 2 escolhida\n");
-            break;
-        case 3:
-            printf("Opcao 3 escolhida\n");
-            break;
-        default:
-            printf("Opcao invalida\n");
-            break;
-    }
-
-    return 0;
+if (x < 10 && y != 0) { 
+    x = x + 1; 
+} else { 
+    x = 0;
 }
--- code_examples/ex2.c
+++
@@ -1,7 +0,0 @@
-let x = 42;
-let y = 3.14;
-let c = 'a';
-x = x + 10;
-y = y * 2
-c = "\n";
-return x;
